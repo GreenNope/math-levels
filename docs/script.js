0c6269b0b0f48e16ee1647f513327ce61925c58b
GreenNope <tlash1000@gmail.com>
2018-09-14 10:54:14 -0400

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -3,15 +3,20 @@ var addUpgrades = 0;
 
 
 function updateText() {
-  document.getElementById("nValue").innerHtml11 = nValue;
+  document.getElementById("nValue").innerHtml = nValue;
 }
 
 function buyAddUpgrade(amt) {
-  for (var i=addUpgrades + 1; i <= addUpgrades + amt; i++) {    
-    nValue = nValue - (Math.pow(1.1, i) * 10);
+  var price = 0
+  for (var i=addUpgrades + 1; i <= addUpgrades + amt; i++) {
+    
+    price = price + (Math.pow(1.1, i) * 10);
   }
   
-  addUpgrades = addUpgrades + amt;
+  if (nValue >= price) {
+    addUpgrades = addUpgrades + amt;
+    nValue = nValue - price;
+  }
 }
 
 while (true) {
